2fff6aa0c28a7b0b4b56d99a42b05804420a51fe
Jakub47 <jakub7447@gmail.com>
2018-04-24 23:40:09 +0200

    Project has been created. Start adding models,con,and sW

---
+++ Project/SDA/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SDA.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public int DormitoryId { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+    }
+}
